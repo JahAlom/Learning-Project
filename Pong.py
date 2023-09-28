a9c948893e54b47306a7fc54d934451fd988d652
Jahurul Alom <jahurulalom@gmail.com>
2023-09-28 00:31:52 -0400

    Added "pong" response and method to retrieve user's machine name

--- Pong.py
+++ Pong.py
@@ -1,11 +1,17 @@
 from flask import Flask #importing Flask
 from datetime import datetime #importing datetime module
+import socket #importing socket module
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    now = datetime.now()
-    return str(now)
+def index(): #Index function to return pong response, current time, and name of local machine
+    now = datetime.now() #current time using datetime.now method to variable
+    message = "pong" #pong response to string variable
+    machine = socket.gethostname() #name of machine using socket.gethostname() method to variable
+    return [str(now), message, machine]; #returns variables in list
+
+response = index() #index function data stored inside variable
+print(response) #data of response printed
     
 if __name__ == "__main__":
 	app.run()
